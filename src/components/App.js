7615709a1f83f5376b01bb1757e98810f963a0f5
Cambisin <cameron_ewart17@hotmail.com>
2023-01-11 20:33:02 +0000

    Forecast Summaries

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LocationDetails from './LocationDetails';
+import ForecastSummaries from './ForecastSummaries';
+
+import '../styles/App.css';
+
+function App({location, forecasts}) {
+  return (
+    <div className="weather-app">
+      <LocationDetails
+        city={location.city}
+        country={location.country}
+      />
+
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
+};
+
+App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
